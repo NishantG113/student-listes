ea20be970aa6b54135dc1df3ff33e95937a29a43
NISHANT NISHANT <nishant8@shernet.sheridancollege.ca>
2025-01-22 09:42:13 -0500

    second branch

---
+++ src/week2/LocalStudent.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package week2;
+
+/**
+ *
+ * @author Dell
+ */
+public class LocalStudent extends Week2{
+    public int numOfCourse;
+    
+}
